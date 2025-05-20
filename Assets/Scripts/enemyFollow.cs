a97c32ba5cb3e2f07cba2adf1f64bc674e2191fe
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-05-20 20:59:14 -0300

    Updates on the skull enemy

---
+++ Assets/Scripts/enemyFollow.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using System.Collections;
+
+public class EnemyFollow : MonoBehaviour {
+	public float moveSpeed = 5f;		// enemy move speed
+	public GameObject target;
+	private Transform targetPos;
+
+	void Awake() {
+		if(target == null) {
+			targetPos = GameObject.Find("Player").transform;
+		}else{
+			targetPos = GameObject.Find("Player").transform;
+		}
+	}
+	
+	void EnemyMovement() {
+		if(Vector3.Distance(transform.position, targetPos.position) > 0.1 && Vector3.Distance(transform.position, targetPos.position) < 10) {
+			transform.position = Vector3.MoveTowards(transform.position, targetPos.position, moveSpeed * Time.deltaTime);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			EnemyMovement();
+	}
+}
